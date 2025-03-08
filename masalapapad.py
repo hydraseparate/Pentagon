f1bf42c2b5f6712344290c6f6c0790a23f4872cc
oksin1 <kishorok16@gmail.com>
2025-03-08 13:33:46 +0530

    Papad for sale

---
+++ masalapapad.py
@@ -0,0 +1,10 @@
+print("masala papad for 10rs")
+print("go and get one")
+i = 0
+while True:
+    print(i+1,"Sold")
+    i+=1
+    if i == 10:
+        print("all sold")
+        break
+print("great papad")
